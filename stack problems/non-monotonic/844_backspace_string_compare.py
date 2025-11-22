ce95e3b68828ea21125ea2649798740adc784719
Anton Simeonov <aplamenov@gmail.com>
2025-11-22 14:19:03 +0200

    same idea but with one function that returns the backspaced strings

--- stack problems/non-monotonic/844_backspace_string_compare.py
+++ stack problems/non-monotonic/844_backspace_string_compare.py
@@ -1,22 +1,36 @@
 def backspace_compare(s: str, t: str) -> bool:
-    s_stack=[]
-    # backspace first string
-    for el in s:
-        if el == '#' and s_stack:
-            s_stack.pop()
-        elif el != '#':
-            s_stack.append(el)
-    t_stack = []
-    # backspace first string
-    for el in t:
+    # s_stack=[]
+    # # backspace first string
+    # for el in s:
+    #     if el == '#' and s_stack:
+    #         s_stack.pop()
+    #     elif el != '#':
+    #         s_stack.append(el)
+    # t_stack = []
+    # # backspace first string
+    # for el in t:
+    #     # if element is # then pop last element from stack
+    #     if el == '#' and t_stack:
+    #         t_stack.pop()
+    #     elif el != "#":
+    #         t_stack.append(el)
+
+    # return t_stack == s_stack
+
+    # with function
+    def backspaced_string(string):
+        string_stack = []
+        for el in string:
         # if element is # then pop last element from stack
-        if el == '#' and t_stack:
-            t_stack.pop()
-        elif el != "#":
-            t_stack.append(el)
-
-
-    return t_stack == s_stack
+            if el == '#' and string_stack:
+                string_stack.pop()
+            # if el is not # append to stack
+            elif el != "#":
+                string_stack.append(el)
+        # return the backspaced string
+        return string_stack
+        # compare the two backspaced strings returned from the method and return True or False
+    return backspaced_string(s) == backspaced_string(t)
 
 print(backspace_compare(s = "ab#c", t = "ad#c")) # True
 print(backspace_compare(s = "ab##", t = "c#d#")) # True
