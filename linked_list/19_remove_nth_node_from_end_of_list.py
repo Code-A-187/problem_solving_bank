3a0d4a85a498a3ac2f66d6c5b21a56e456e26326
Anton Simeonov <aplamenov@gmail.com>
2025-12-15 11:08:11 +0200

    with setup to start the function

--- linked_list/19_remove_nth_node_from_end_of_list.py
+++ linked_list/19_remove_nth_node_from_end_of_list.py
@@ -4,9 +4,8 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        m = 0
 
-def remove_nth_node_from_end(head: Optional[ListNode], n: int) -> Optional[ListNode]:
+def remove_nth_from_end(head: Optional[ListNode], n: int) -> Optional[ListNode]:
     dummy = ListNode(0, head)
     slow = dummy
     fast = head
@@ -24,4 +23,24 @@ def remove_nth_node_from_end(head: Optional[ListNode], n: int) -> Optional[ListN
     return dummy.next
         
 
+node5 = ListNode(5)
+node4 = ListNode(4, next=node5) # 4 -> 5
+node3 = ListNode(3, next=node4) # 3 -> 4 -> 5
+node2 = ListNode(2, next=node3) # 2 -> 3 -> 4 -> 5
+node1 = ListNode(1, next=node2) # 1 -> 2 -> 3 -> 4 -> 5
+head = node1 # entry point to the list
 
+n = 2
+
+# call the function
+new_head = remove_nth_from_end(head, n)
+
+# print the Result Manually
+print(f"List after removing the {n}nd node from the end:")
+current = new_head
+output = []
+while current:
+    output.append(current.val)
+    current = current.next
+
+print(output)
